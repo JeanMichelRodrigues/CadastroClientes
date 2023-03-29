56e66eaaaa1e0ed598befb8a8d9dcb2b2c6a0bbd
JeanmRodrigues <jean.rodrigues@benner.com.br>
2023-03-29 09:28:35 -0300

    first commit

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Cadastro()
+        {
+            ViewBag.Message = "Cadastro de Clientes";
+
+            return View();
+        }
+        public ActionResult Home()
+        {
+            ViewBag.Message = "Home";
+
+            return View();
+        }
+    }
+}
